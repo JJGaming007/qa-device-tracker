4cda224bfd467070b6b068a1919dece161c59e50
Jibin174 <jibin.jose@supergaming.com>
2025-05-20 23:55:35 +0530

    WORKER TIMEOUT + SIGKILL and OperationalError: SSL error

--- app.py
+++ app.py
@@ -454,4 +454,4 @@ def api_return_device():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=5000)
